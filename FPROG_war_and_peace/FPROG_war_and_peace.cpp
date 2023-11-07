7a3e8657fd49b1daf9dc4f478ace088969cc12d4
gawdkeno <if21b032@technikum-wien.at>
2023-11-07 14:20:25 +0100

    Add project files.

---
+++ FPROG_war_and_peace/FPROG_war_and_peace.cpp
@@ -0,0 +1,12 @@
+﻿// FPROG_war_and_peace.cpp : Defines the entry point for the application.
+//
+
+#include "FPROG_war_and_peace.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
