c6b06dc958e854d04974d7238ad4dafc9e60f1f8
gawdkeno <if21b032@technikum-wien.at>
2023-11-09 18:56:43 +0100

    [#2]  added readFile function

--- FPROG_war_and_peace/FPROG_war_and_peace.cpp
+++ FPROG_war_and_peace/FPROG_war_and_peace.cpp
@@ -10,10 +10,42 @@
 
 #include "FPROG_war_and_peace.h"
 
-using namespace std;
+auto readFile = [](const std::string& filename) -> std::vector<std::string> 
+{
+    std::ifstream file(filename);
+    if (!file.is_open()) 
+    {
+        throw std::runtime_error("Could not open file: " + filename);
+    }
+    std::vector<std::string> lines;
+    std::string line;
+    while (std::getline(file, line)) 
+    {
+        lines.push_back(line);
+    }
+    return lines;
+};
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+    std::string book = "../../../../files/war_and_peace.txt";
+    std::string warWordList = "../../../../files/peace_terms.txt";
+    std::string peaceWordList = "../../../../files/war_terms.txt";
+    try 
+    {
+        auto bookLines = readFile(book);
+        auto warTerms = readFile(warWordList);
+        auto peaceTerms = readFile(peaceWordList);
+
+        std::cout << "\nread files successfully\n";
+        // rest of the processing will go here
+
+    }
+    catch (const std::exception& e) 
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
